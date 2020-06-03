e4035547fdaa915a8fe51f876030780a86f87374
LMachart <louis.machart19@gmail.com>
2020-06-03 10:04:46 +0200

    test for a little array

--- demo.php
+++ demo.php
@@ -1,10 +1,4 @@
 <?php
-$prenom = 'Louis';
-$nom = 'Machart';
-$note1 = 20;
-$note2 = 10;
-$moyenne = ($note1 + $note2) /2;
 
-echo 'Bonjour ' . $prenom . ' ' . $nom  .  ' vous avez eu ' . ($note1 + $note2) /2 .  ' de moyenne';
-
-echo "\nBonjour $prenom $nom vous avez eu $moyenne de moyenne";
+$notes = [10, 20, 9, 8, 17,5];
+echo $notes [3];
