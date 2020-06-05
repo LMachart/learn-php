b86ad4f6c7654a893eca8fbcea99fc695313b424
LMachart <louis.machart19@gmail.com>
2020-06-05 10:20:56 +0200

    add a readline

--- demo.php
+++ demo.php
@@ -1,15 +1,8 @@
 <?php
 
-$classe = [
-    [
-        'nom' => 'Machart',
-        'prenom' => 'Louis' ,
-        'notes'=> [18, 18, 18]
-    ],
-    [
-        'nom' => 'Machart' ,
-        'prenom' => 'Jean' ,
-        'notes' => [17, 10, 14]
-    ]
-    ];
-    echo $classe[1]['notes'][1];
+$note = readline ('Entrez votre note : ');
+if ($note >= 10) {
+    echo 'Bravo vous avez la moyenne';
+} else {
+     echo 'Dommage vous n\'avez pas la moyenne';
+}
