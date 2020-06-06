08f1dbbebae70bd27b4853eae6de6786a3da4aa6
LMachart <louis.machart19@gmail.com>
2020-06-06 10:33:29 +0200

    add a condition

--- demo.php
+++ demo.php
@@ -1,8 +1,12 @@
 <?php
 
-$note = readline ('Entrez votre note : ');
-if ($note >= 10) {
+$note = (int) readline ('Entrez votre note :');
+if ($note > 10) {
     echo 'Bravo vous avez la moyenne';
+} elseif ($note === 10) {
+    
+    echo 'Vous avez tout juste la moyenne';
+
 } else {
      echo 'Dommage vous n\'avez pas la moyenne';
 }
