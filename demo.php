e04c4b1ae805a29aec88584727d35a0058e9a505
LMachart <louis.machart19@gmail.com>
2020-06-09 11:04:55 +0200

    add the same code with the else and elseif and add commentary switch

--- demo.php
+++ demo.php
@@ -2,6 +2,24 @@
 
 $action = (int) readline ('Entrez votre action  : ( 1: attaquer, 2: defendre, 3: passer mon tour)'  );
 
+if ($action === 1){
+    echo 'J\'attaque';
+} elseif ($action === 2) {
+    echo 'je defends';
+} elseif ($action === 3){
+    echo 'je ne fais rien';
+} else {
+    echo 'commande inconnu';
+}
+
+
+
+
+
+
+
+
+/*
 switch ($action) {
     case 1:
         echo 'J\'attaque';
@@ -17,4 +35,4 @@ switch ($action) {
 
     default: 
     echo 'commande inconnu';
-}
+}*/
