e74d209db51e53dd310cd78037b9b805adc6f987
LMachart <louis.machart19@gmail.com>
2020-06-11 10:45:08 +0200

    test for while boucle

--- demo.php
+++ demo.php
@@ -1,38 +1,8 @@
 <?php
+$chiffre = null; 
 
-$action = (int) readline ('Entrez votre action  : ( 1: attaquer, 2: defendre, 3: passer mon tour)'  );
-
-if ($action === 1){
-    echo 'J\'attaque';
-} elseif ($action === 2) {
-    echo 'je defends';
-} elseif ($action === 3){
-    echo 'je ne fais rien';
-} else {
-    echo 'commande inconnu';
+while ($chiffre !== 10) {
+    $chiffre = (int) readline ('Entrez une heure : '); 
 }
 
-
-
-
-
-
-
-
-/*
-switch ($action) {
-    case 1:
-        echo 'J\'attaque';
-    break;
-
-    case 2: 
-        echo 'je defends';
-    break;
-    
-    case 3: 
-        echo 'je ne fais rien';
-    break;
-
-    default: 
-    echo 'commande inconnu';
-}*/
+echo 'Bravo vous avez gagné';
