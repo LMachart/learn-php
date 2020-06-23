85591916d00987148aa22b03f604742ffa1abeb6
LMachart <louis.machart19@gmail.com>
2020-06-23 11:04:28 +0200

    test for each

--- demo.php
+++ demo.php
@@ -1,5 +1,14 @@
 <?php
 
-for ($i = 0; $i < 10; $i++) {
-    echo "- $i   \n";
+$eleves = [
+    'cm2' => ['Jean', 'Marc', 'Louis'],
+    'cm1' => [ 'Jeanne', 'Louise']
+];
+
+foreach ($eleves as  $classe => $listEleves) {
+    echo "La classe $classe: \n";
+    foreach ($listEleves as $eleve) {
+        echo "- $eleve\n";
+    }
+    echo "\n";
 }
