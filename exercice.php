b731eb65c83b866f8c9e43fcd75f865d9e736fde
LMachart <louis.machart19@gmail.com>
2020-07-03 09:35:43 +0200

    add a little exercise

---
+++ exercice.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+Demande à l'utilisateur de rentrer une note ou de taper "fin"  chaque note est sauvegardée dans un tableau $notes (pensez $notes[])
+à la fin n affiche le tableau de note sous forme de liste 
+*/
+
+
+
+
+
+// Tant que l'utilisateur ne tape pas "fin" 
+// on rajoute la notes tapée dans le tableai notes 
+// Pour chaque note dans notes 
+// on affiche "- notes 
